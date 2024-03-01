b8716d3e6c7342141fda5d7c827d3ac3d21cba13
tonysub7 <114236250+tonysub7@users.noreply.github.com>
2024-03-01 11:07:08 +0100

    Create setup_ufw_ethrpc.sh

---
+++ setup_ufw_ethrpc.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Ensure the script is run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Default policies (adjust if necessary)
+ufw default deny incoming
+ufw default allow outgoing
+
+# Allow trafic for P2P
+ufw allow 30303
+ufw allow 9000
+
+# Allow all traffic from the Tailscale
+ufw allow from 100.0.0.0/8
+
+# Allow VPSs to access the RPCs
+ufw allow from 149.50.135.15 to any port 443
+ufw allow from 158.69.121.222 to any port 443
+
+# Specific port rules for denying
+ufw deny 8545
+ufw deny 8546
+ufw deny 8551
+ufw deny 5052
+ufw deny 3000
+ufw deny 9090
+ufw deny 9900
+ufw deny 9901
+ufw deny 9902
+ufw deny 80
+ufw deny 443
+
+# Enable UFW
+ufw --force enable
+
+# Reload UFW to apply changes
+ufw reload
+
+# Display status
+ufw status numbered
